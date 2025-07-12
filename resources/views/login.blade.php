36678c93789ef43ab707005e1aec7365592cb8f1
Ali Shahzad <alishahzad9054933@gmail.com>
2025-07-12 16:25:19 +0500

    dashboard files added

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,47 +1,197 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+    <title>Laravel</title>
 
-        <!-- Styles / Scripts -->
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+
+    <!-- Styles / Scripts -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('style.css') }}">
-    </body>
+    <style>
+        /* From Uiverse.io by Smit-Prajapati */
+        .container {
+            max-width: 350px;
+            background: #F8F9FD;
+            background: linear-gradient(0deg, rgb(255, 255, 255) 0%, rgb(244, 247, 251) 100%);
+            border-radius: 40px;
+            padding: 25px 35px;
+            border: 5px solid rgb(255, 255, 255);
+            box-shadow: rgba(235, 200, 6, 0.88) 0px 30px 30px -20px;
+            margin: 20px;
+            
+        }
+
+        .heading {
+            text-align: center;
+            font-weight: 900;
+            font-size: 30px;
+            color: rgb(255, 153, 0);
+        }
+
+        .form {
+            margin-top: 20px;
+            align-content: center;
+        }
+
+        .form .input {
+            width: 100%;
+            background: white;
+            border: none;
+            padding: 15px 20px;
+            border-radius: 20px;
+            margin-top: 15px;
+            box-shadow: #f6ffcfff 0px 10px 10px -5px;
+            border-inline: 2px solid transparent;
+        }
+
+        .form .input::-moz-placeholder {
+            color: rgb(170, 170, 170);
+        }
+
+        .form .input::placeholder {
+            color: rgb(170, 170, 170);
+        }
+
+        .form .input:focus {
+            outline: none;
+            border-inline: 2px  rgb(255, 153, 0);
+        }
+
+        .form .forgot-password {
+            display: block;
+            margin-top: 10px;
+            margin-left: 10px;
+        }
+
+        .form .forgot-password a {
+            font-size: 11px;
+            color:  rgb(255, 153, 0);
+            text-decoration: none;
+        }
+
+        .form .login-button {
+            display: block;
+            width: 100%;
+            font-weight: bold;
+            background: linear-gradient(45deg,  rgb(255, 153, 0) 0%, rgba(248, 192, 7, 0.79) 100%);
+            color: white;
+            padding-block: 15px;
+            margin: 20px auto;
+            border-radius: 20px;
+            box-shadow: rgba(238, 156, 4, 0.94) 0px 20px 10px -15px;
+            border: none;
+            transition: all 0.2s ease-in-out;
+        }
+
+        .form .login-button:hover {
+            transform: scale(1.03);
+            box-shadow:rgba(238, 156, 4, 0.94) 0px 23px 10px -20px;
+        }
+
+        .form .login-button:active {
+            transform: scale(0.95);
+            box-shadow: rgba(238, 156, 4, 0.94) 0px 15px 10px -10px;
+        }
+
+        .social-account-container {
+            margin-top: 25px;
+        }
+
+        .social-account-container .title {
+            display: block;
+            text-align: center;
+            font-size: 10px;
+            color:rgba(238, 156, 4, 0.94)
+        }
+
+        .social-account-container .social-accounts {
+            width: 100%;
+            display: flex;
+            justify-content: center;
+            gap: 15px;
+            margin-top: 5px;
+        }
+
+        .social-account-container .social-accounts .social-button {
+            background: linear-gradient(45deg, rgb(0, 0, 0) 0%, rgb(112, 112, 112) 100%);
+            border: 5px solid white;
+            padding: 5px;
+            border-radius: 50%;
+            width: 40px;
+            aspect-ratio: 1;
+            display: grid;
+            place-content: center;
+            box-shadow: rgba(133, 189, 215, 0.8784313725) 0px 12px 10px -8px;
+            transition: all 0.2s ease-in-out;
+        }
+
+        .social-account-container .social-accounts .social-button .svg {
+            fill: white;
+            margin: auto;
+        }
+
+        .social-account-container .social-accounts .social-button:hover {
+            transform: scale(1.2);
+        }
+
+        .social-account-container .social-accounts .social-button:active {
+            transform: scale(0.9);
+        }
+
+        .agreement {
+            display: block;
+            text-align: center;
+            margin-top: 15px;
+        }
+
+        .agreement a {
+            text-decoration: none;
+            color:  rgb(255, 153, 0);
+            font-size: 9px;
+        }
+    </style>
 </head>
 
 <body>
     <div class="form-back">
-        <div class="container">
-            <h3>Login Page</h3>
-            <form method="POST" action="{{ route('login') }}">
+        <div  class="container">
+            <div class="heading">Sign In</div>
+            <form method="POST" action="{{ route('login') }}" class="form">
                 @csrf
-                
-                <div class="mb-3">
-                    <label for="exampleInputText" class="form-label">Email</label>
-                    <input type="email" class="form-control" id="exampleInputText" name="email" id="email">
-                </div>
-                <div class="mb-3">
-                    <label for="exampleInputImage2" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="exampleInputImage2" name="password" id="password">
-                </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+
+                <input required="" class="input" type="email" name="email" id="email" placeholder="E-mail">
+                <input required="" class="input" type="password" name="password" id="password" placeholder="Password">
+                <span class="forgot-password"><a href="#">Forgot Password ?</a></span>
+                <input class="login-button" type="submit" value="Sign In">
+
             </form>
+            <!-- <form method="POST" action="{{ route('login') }}">
+                    @csrf
+
+                    <div class="mb-3">
+                        <input required="" class="input" type="email" name="email" id="email" placeholder="E-mail">
+                    </div>
+                    <div class="mb-3">
+                        <input required="" class="input" type="password" name="password" id="password">
+                    </div>
+                    <input class="login-button" type="submit" value="Sign In">
+                </form> -->
         </div>
         @if ($errors->any())
-        <div class="alert text-danger">
+            <div class="alert text-danger">
 
-            @foreach ($errors-> all() as $error)
-            <p>{{$error}}</p>
-            @endforeach
-        </div>
+                @foreach ($errors->all() as $error)
+                    <p>{{$error}}</p>
+                @endforeach
+            </div>
         @endif
     </div>
 
